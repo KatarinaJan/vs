a48c5dc656799a7f979328f4747d837cdb5e4682
Jan <15779649834@163.com>
2020-10-08 16:58:47 +0800

    <新增> 整合权限管理模块

--- guli-admin/config/dev.env.js
+++ guli-admin/config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:9001"',
+  BASE_API: '"http://localhost:8222"',
   OSS_PATH: '"https://guli-file4.oss-cn-shanghai.aliyuncs.com"'
 })
